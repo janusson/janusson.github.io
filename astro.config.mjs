245ab2f91a687d410cc3eaa7616dcac7619d5fd0
janussone <ericjanusson@outlook.com>
2026-06-29 21:37:39 -0700

    Migrate to Astro 7 and add Tailwind CSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ericjanusson.ca",
   integrations: [mdx()],
-  markdown: {
+
+  markdown: unified({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
